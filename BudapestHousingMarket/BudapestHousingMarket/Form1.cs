a8f0cebc3574f3b19b0fe8eb899b1628362633b5
XQLWL1 <vivien.sebestyen@stud.uni-corvinus.hu>
2021-11-15 12:03:37 +0100

    reference to the excel object

--- BudapestHousingMarket/BudapestHousingMarket/Form1.cs
+++ BudapestHousingMarket/BudapestHousingMarket/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace BudapestHousingMarket
 {
@@ -19,6 +21,7 @@ namespace BudapestHousingMarket
         {
             InitializeComponent();
             LoadData();
+
         }
 
         private void LoadData()
