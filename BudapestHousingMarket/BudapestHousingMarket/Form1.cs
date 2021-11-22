25ce6527894da8bc39298bfe82272cf66ff79410
XQLWL1 <vivien.sebestyen@stud.uni-corvinus.hu>
2021-11-22 14:54:52 +0100

    Program ellenőrzése, javtása

--- BudapestHousingMarket/BudapestHousingMarket/Form1.cs
+++ BudapestHousingMarket/BudapestHousingMarket/Form1.cs
@@ -15,8 +15,9 @@ namespace BudapestHousingMarket
 {
     public partial class Form1 : Form
     {
-        RealEstateEntities contex = new RealEstateEntities();
+
         List<Flat> Flats;
+        RealEstateEntities contex = new RealEstateEntities();
 
         Excel.Application xlApp;
         Excel.Workbook xlWB;
@@ -35,10 +36,10 @@ namespace BudapestHousingMarket
             try
             {
                 // Excel elindítása és az applikáció objektum betöltése
-                Excel.Application xlApp = new Excel.Application();
+                xlApp = new Excel.Application();
 
                 // Új munkafüzet létrehozása
-                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlWB = xlApp.Workbooks.Add();
 
                 // Új munkalap beillesztése az excelbe
                 xlSheet = xlWB.ActiveSheet;
@@ -49,13 +50,15 @@ namespace BudapestHousingMarket
 
                 //felhasználók számára elérhetővé válik
                 xlApp.Visible = true;
+
+                //Tiltani és engedélyezni tudjuk az excel alkamazásnak a felhasználó általi vezérlését.
                 xlApp.UserControl = true;
             }
             catch (Exception exception)
             {
 
                 string errorMsg = string.Format("Error: {0}\nLine: {1}", exception.Message, exception.Source);
-                MessageBox.Show(errorMsg, "Error");
+                MessageBox.Show(errorMsg);
 
                 xlWB.Close(false, Type.Missing, Type.Missing);
                 xlApp.Quit();
@@ -67,7 +70,7 @@ namespace BudapestHousingMarket
 
         private void CreateTable()
         {
-            string[] headers = new string[] 
+            string[] headers = new string[]
             {
                 "Kód",
                 "Eladó",
@@ -81,55 +84,52 @@ namespace BudapestHousingMarket
             };
 
             for (int i = 0; i < headers.Length; i++)
+
+                xlSheet.Cells[1, i+1] = headers[i];
+
+            object[,] values = new object[Flats.Count, headers.Length];
+            int counter = 0;
+            foreach (Flat item in Flats)
             {
-                xlSheet.Cells[1, i] = headers[i-1];
+                values[counter, 0] = item.Code;
+                values[counter, 1] = item.Vendor;
+                values[counter, 2] = item.Side;
+                values[counter, 3] = item.District;
+                values[counter, 4] = item.Elevator;
+                values[counter, 5] = item.NumberOfRooms;
+                values[counter, 6] = item.FloorArea;
+                values[counter, 7] = item.Price;
 
-                object[,] values = new object[Flats.Count, headers.Length];
-
-                int counter = 0;
-                foreach (Flat item in Flats)
-                {
-                    values[counter, 0] = item.Code;
-                    values[counter, 1] = item.Vendor;
-                    values[counter, 2] = item.Side;
-                    values[counter, 3] = item.District;
-                    values[counter, 4] = item.Elevator;
-                    values[counter, 5] = item.NumberOfRooms;
-                    values[counter, 6] = item.FloorArea;
-                    values[counter, 7] = item.Price;
-
-                    //Ezt a sort megnézni!!:
-                    values[counter, 8] = "="+GetCell(counter+2,8)+"*1000000/"+GetCell(counter+2, 7);
-                    counter++;
-                }
-
-                xlSheet.get_Range
-                    (
-                        GetCell(2, 1),
-                        GetCell(1 + values.GetLength(0), values.GetLength(1))
-                    ).Value2 = values;
-
-
-                Excel.Range headerRange= xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
-                headerRange.Font.Bold = true;
-
-                //fejléc: középre rendezés függőlegesen és vizszintesen is
-                headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
-                headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
-
-                headerRange.EntireColumn.AutoFit();
-
-                //nagyíthajuk:
-                headerRange.RowHeight = 60;
-
-                //szinezhetjük
-                headerRange.Interior.Color = Color.Salmon;
-
-                //körbe keretezzük a fejlécet
-                headerRange.BorderAround2(Excel.XlLineStyle.xlDashDotDot, Excel.XlBorderWeight.xlThick);
-
-
+                values[counter, 8] = "=" + GetCell(counter + 2, 8) + "*1000000/" + GetCell(counter + 2, 7);
+                counter++;
             }
+
+            xlSheet.get_Range
+                (
+                    GetCell(2, 1),
+                    GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+
+
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+
+            //fejléc: középre rendezés függőlegesen és vizszintesen is
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+
+            headerRange.EntireColumn.AutoFit();
+
+            //nagyíthajuk:
+            headerRange.RowHeight = 60;
+
+            //szinezhetjük
+            headerRange.Interior.Color = Color.Salmon;
+
+            //körbe keretezzük a fejlécet
+            headerRange.BorderAround2(Excel.XlLineStyle.xlDashDotDot, Excel.XlBorderWeight.xlThick);
+
+
+
         }
 
         private void LoadData()
